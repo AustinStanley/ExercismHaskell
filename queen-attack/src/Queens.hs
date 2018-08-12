b80b5baa84e45c5269d788a0f697047d814aa3ac
Austin Stanley <r.austin.stanley@gmail.com>
2018-08-12 14:45:28 -0500

    Queens exercise

---
+++ queen-attack/src/Queens.hs
@@ -0,0 +1,27 @@
+module Queens (boardString, canAttack) where
+
+boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
+boardString Nothing Nothing = unlines ["_ _ _ _ _ _ _ _",
+                                       "_ _ _ _ _ _ _ _",
+                                       "_ _ _ _ _ _ _ _",
+                                       "_ _ _ _ _ _ _ _",
+                                       "_ _ _ _ _ _ _ _",
+                                       "_ _ _ _ _ _ _ _",
+                                       "_ _ _ _ _ _ _ _",
+                                       "_ _ _ _ _ _ _ _"]
+
+boardString white black = replaceAt white "W" . replaceAt black "B" $ boardString Nothing Nothing
+    where replaceAt Nothing _ str = str
+          replaceAt (Just (a, b)) c str = 
+              let idx = (a * 16 + ((filter even [0..]) !! b))
+                  splitStr = splitAt idx str 
+              in  (fst splitStr) ++ (c ++ (tail . snd $ splitStr))
+
+canAttack :: (Int, Int) -> (Int, Int) -> Bool
+canAttack queenA queenB = isCard || isDiag 
+    where isCard = fst queenA == fst queenB || snd queenA == snd queenB
+          isDiag = queenA `elem` (diags queenB)
+              where diags (a, b) = foldl (\acc i -> acc ++ [(x, y) | x <- [a + i, a - i], 
+                                                                     y <- [b + i, b - i], 
+                                                                     x `elem` [1..8], 
+                                                                     y `elem` [1..8]]) [] [1..8]
